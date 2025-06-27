4d7487ddf1bc8d2825ce27c79e18348cbe6d6c09
vairagi-tech <abhaykumaraks7226@gmail.com>
2025-06-27 10:18:49 +0530

    bug fix

--- Frontend/src/pages/Login.js
+++ Frontend/src/pages/Login.js
@@ -5,11 +5,10 @@ import axios from "axios";
 import Loder from "../components/Loder";
 import "../styles/Loginpage.css";
 const Login = () => {
-  // const img =
-  //   "https://images.unsplash.com/photo-1593538312308-d4c29d8dc7f1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80";
+  
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
-  //from submit
+  
   const submitHandler = async (values) => {
     try {
       setLoading(true);
@@ -27,8 +26,7 @@ const Login = () => {
       message.error(error.response?.data?.message || "Something went wrong with login.");
     }
   };
-
-  //prevent for login user
+  
   useEffect(() => {
     if (localStorage.getItem("user")) {
       navigate("/");
@@ -53,7 +51,7 @@ const Login = () => {
               <Link to="/register">
                 Not a user ? Click Here to regsiter !
               </Link>
-              <button className="btn">Login</button>
+              <button className="btn" style={{ backgroundColor: "#b87333", color: "#fff" }}>Login</button>
             </div>
           </Form>
         </div>
